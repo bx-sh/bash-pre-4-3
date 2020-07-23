8856f8e9afb373a0bb05fabe1b330ff31555b0d2
Rebecca Taylor <bex@beccasaur.us>
2020-07-23 01:42:08 -0700

    $BASH_PRE_4_3

---
+++ main.sh
@@ -0,0 +1,17 @@
+_set_bash_pre_4_3() {
+  local bash_major_version="${BASH_VERSION:0:1}"
+  local bash_minor_version="${BASH_VERSION:2:1}"
+  if [ $bash_major_version -lt 4 ]
+  then
+    export BASH_PRE_4_3=true
+  elif [ $bash_major_version -eq 4 ] && [ $bash_minor_version -lt 3 ]
+  then
+    export BASH_PRE_4_3=true
+  else
+    :
+  fi
+}
+
+_set_bash_pre_4_3
+
+unset -f _set_bash_pre_4_3
